29eff1414e7f8b556fe794d88964805c0393d596
z2typ <102999319+z2typ@users.noreply.github.com>
2022-07-08 19:41:05 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (47.19%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -5,12 +5,16 @@ public:
         vector<int> ans(n);
         if(n==1) return nums[0];
         else if(n==0) return 0;
-        ans[0] = nums[0];
-        ans[1] = max(nums[0],nums[1]);
+        
+        int a = nums[0];
+        int b = max(nums[0],nums[1]);
+        
         for(int i=2;i<n;i++){
-            ans[i] = max(ans[i-2]+nums[i], ans[i-1]);
+            int temp = max(a+nums[i], b);
+            a = b;
+            b = temp;
         }
         
-        return max(ans[n-1],ans[n-2]);
+        return max(a,b);
     }
 };
