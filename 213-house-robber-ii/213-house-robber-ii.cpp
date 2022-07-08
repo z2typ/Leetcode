717abfabf3e740b15cd48c18e4e28bfcb1be1314
z2typ <102999319+z2typ@users.noreply.github.com>
2022-07-08 20:06:17 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (18.41%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int util(vector<int>& nums) {
-        int n = nums.size();
+    int util(vector<int>& nums, int left,int right) {
+        int n = right-left+1;
         vector<int> ans(n);
-        if(n==1) return nums[0];
+        if(n==1) return nums[left];
         else if(n==0) return 0;
         
-        int a = nums[0];
-        int b = max(nums[0],nums[1]);
+        int a = nums[left];
+        int b = max(nums[left],nums[left+1]);
         
         for(int i=2;i<n;i++){
-            int temp = max(a+nums[i], b);
+            int temp = max(a+nums[left+i], b);
             a = b;
             b = temp;
         }
-        
         return max(a,b);
     }
     
@@ -22,10 +21,6 @@ public:
         int n = nums.size();
         if(n==0) return 0;
         else if(n==1) return nums[0];
-        vector<int> a = nums;
-        a.erase(a.begin());
-        vector<int> b = nums;
-        b.pop_back();
-        return max(util(a),util(b));
+        return max(util(nums,0,n-2),util(nums,1,n-1));
     }
 };
