e6bfa2dc01202cddcc3fe2a97f3d967095bcfe8c
z2typ <102999319+z2typ@users.noreply.github.com>
2022-07-11 06:01:05 +0530

    Time: 99 ms (5.13%), Space: 7.7 MB (71.59%) - LeetHub

---
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n = s.length();
+
+        
+        string rev(s);
+        reverse(rev.begin(), rev.end());
+        string s_new = s + "#" + rev;
+        int n_new = s_new.size();
+        
+        int LPS[n_new];
+        LPS[0] = 0;
+        int prevLPS = 0, i = 1;
+        
+        while(i<n_new){
+            if(s_new[prevLPS]==s_new[i]){
+                LPS[i] = prevLPS+1;
+                i++;
+                prevLPS++;
+            }else if(prevLPS==0){
+                LPS[i] = 0;
+                i++;
+            }else{
+                prevLPS = LPS[prevLPS-1];
+            }
+        }
+        
+        for(int x:LPS){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+        
+        return rev.substr(0, n - LPS[n_new - 1]) + s;
+        
+    }
+};
