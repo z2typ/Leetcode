e78ea25e01fea20661c7685181bca5464f041dfd
z2typ <102999319+z2typ@users.noreply.github.com>
2022-07-13 00:04:17 +0530

    Time: 465 ms (34.83%), Space: 10.5 MB (44.36%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+
+  
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n+1);
+        
+        for(int i=n-1;i>=0;i--){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]<nums[j]){
+                    dp[i] = max(dp[i],dp[j]);
+                }
+            }
+            dp[i]++;
+        }
+        
+        
+        return *max_element(dp.begin(),dp.end());
+    }
+};
