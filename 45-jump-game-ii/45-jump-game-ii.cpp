d192f68395bed74f65151a699bfe2f92e379c9c9
z2typ <102999319+z2typ@users.noreply.github.com>
2022-07-15 17:15:42 +0530

    Time: 40 ms (50.24%), Space: 16.7 MB (52.98%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        int max_jump = nums[0];
+        int jumps = 0;
+        while(j<n-1){
+            i = j+1;
+            j = max_jump;
+            for(int k=i;k<=min(j,n-1);k++){
+                if(nums[k]+k>max_jump){
+                    max_jump = nums[k]+k;
+                }
+            }
+            jumps++;
+        }
+        return jumps;
+    }
+};
