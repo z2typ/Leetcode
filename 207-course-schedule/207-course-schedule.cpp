d9f4e26e8a9b482bbea743c0b5974660d406c2f7
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-10 11:06:30 +0530

    Time: 40 ms (39.46%), Space: 15.4 MB (15.74%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    unordered_set<int> uset;
+    
+    bool hasCycle(vector<vector<int>> &graph,vector<int> &visited,int start=0){
+        int n = graph.size();
+        uset.insert(start);
+        visited[start] = 1;
+        bool cycle = false;
+        for(int node :graph[start]){
+            if(uset.count(node)){
+                return true;
+            }
+            if(!visited[node]){
+                cycle = cycle || hasCycle(graph,visited,node);
+            }
+        }
+        
+        uset.erase(start);
+        
+        return cycle;    
+        
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses,vector<int>());
+        
+        for(vector<int> preq : prerequisites){
+            graph[preq[0]].push_back(preq[1]);
+        }
+        
+        vector<int> visited(numCourses);
+        
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                uset.clear();
+                if(hasCycle(graph,visited,i)){
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+        
+
+    }
+};
