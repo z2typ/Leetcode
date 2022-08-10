8d5c00b8adf06c949feb41819d6c2224c597e820
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-10 08:47:43 +0530

    Time: 5 ms (11.24%), Space: 5.8 MB (75.53%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        while(b!=0){
+            unsigned carry = a&b;
+            a = a^b;
+            b = carry<<1;
+        }
+        return a;
     }
 };
