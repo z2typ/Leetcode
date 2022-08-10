a63f8b9c524e632779fcc983f0d793db793b1abb
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-11 01:14:22 +0530

    Time: 35 ms (84.39%), Space: 23.3 MB (63.86%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,34 +10,14 @@
 
 class Solution {
 public:
-    int found1 = 0;
-    int found2 = 0;
-    TreeNode* LCA = NULL;
-    void helper(TreeNode* root,TreeNode* p, TreeNode* q,TreeNode* parent){
-        if(root==p || root==q){
-            if(found1!=1){
-                found1=1;
-                LCA = root;
-            }else{
-                found2 = 1;
-            }
-        }
-        if(root->left!=NULL){
-            helper(root->left,p,q,root);
-        }
-        
-        if(root->right!=NULL) { 
-            helper(root->right,p,q,root);
-        }
-        
-        if(found2!=1 && root==LCA){
-            LCA = parent;
-        }
-        
-    }
-    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        helper(root,p,q,NULL);
-        return LCA;
+        while(true)
+        if(root->val < p->val && root->val <q->val){
+            root = root->right;
+        }else if(root->val > p->val && root->val > q->val){
+            root = root->left;
+        }else{
+            return root;
+        }
     }
 };
