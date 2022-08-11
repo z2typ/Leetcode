2867dc86de864521dc7d4a66b3068060f7ac9af8
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-11 20:33:29 +0530

    Time: 288 ms (22.65%), Space: 56 MB (26.39%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> visited;
+    void dfs(vector<vector<int>> &graph, int s){
+        visited[s] = 1;
+        for(int adj_node : graph[s]){
+            if(!visited[adj_node]){
+                dfs(graph, adj_node);
+            }
+        }
+        
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int conn = connections.size();
+        if(conn < (n-1)){
+            return -1;
+        }
+        
+        visited = vector<int>(n);
+        
+        vector<vector<int>> adj(n);
+        for(vector<int> conn : connections){
+            adj[conn[0]].push_back(conn[1]);
+            adj[conn[1]].push_back(conn[0]);
+        }
+        int count = 0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                count++;
+                dfs(adj,i);
+            }
+        }
+        
+        return count-1;
+        
+        
+        
+        
+    }
+};
