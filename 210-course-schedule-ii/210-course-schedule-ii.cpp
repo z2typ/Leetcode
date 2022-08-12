0437a85f19b8c940e3f78f7e05cf70d71db94aae
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-13 01:35:29 +0530

    Time: 31 ms (63.40%), Space: 15.7 MB (11.50%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> topoSort;
+    unordered_set<int> uset;
+    int flag = 0;
+    void dfs(vector<vector<int>> &graph, int node,vector<int> & visited){
+        visited[node] = 1;
+        uset.insert(node);
+        for(int adj_node : graph[node]){
+            if(uset.count(adj_node)){
+                flag = 1;
+            }
+            if(!visited[adj_node]){
+                dfs(graph,adj_node,visited);
+            }
+        }
+        topoSort.push_back(node);
+        uset.erase(node);
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<vector<int>> graph(numCourses);
+        
+        for(vector<int> edge : prerequisites){
+            graph[edge[0]].push_back(edge[1]);
+        }
+        
+        vector<int> visited(numCourses);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i])
+                dfs(graph,i,visited);
+        }
+        if(flag){
+            topoSort.clear();
+        }
+        return topoSort;
+        
+    }
+};
