249cd5153d3ceb343a5bc71a8a6a1b8f4280b045
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-12 19:54:01 +0530

    Time: 3 ms (61.43%), Space: 8.3 MB (8.61%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    int util(string s,int n,vector<int>&t){
-        if(t[n]!=-1) return t[n];
-        if(n==0 || (n==1 && s[0]!='0')) return 1;
-        else if(n==1 && s[0]=='0') return 0;
+    vector<int> t;
+    int numWays(string s,int n){
         
-        int a=0,b=0;
-        
-        if(s[n-1]!='0'){
-            a = util(s,n-1,t);
+        if(n==0){
+            return 1;
+        }
+        if(t[n]!=-1){
+            return t[n];
         }
         
-        if(1<=stoi(s.substr(n-2,2)) && stoi(s.substr(n-2,2))<=26 && s[n-2]!='0'){
-            b = util(s,n-2,t);
+        int a = 0,b = 0;
+        
+        if(0<=n-1 && s[n-1]!='0'){
+            a = numWays(s,n-1);
         }
         
+        if(0<=n-2 && s[n-2]!='0' && 1<= stoi(s.substr(n-2,2)) && stoi(s.substr(n-2,2))<=26){
+            b = numWays(s,n-2);
+        }
         return t[n] = a+b;
-             
+        
     }
     
     int numDecodings(string s) {
-        int n = s.length();
-        vector<int> t(n+1,-1);
-        return util(s,n,t);
+        int n = s.size();
+        t = vector<int>(n+1,-1);
+        return numWays(s,n);
     }
 };
