bab8700e22f7da792ed7cd6d26efad58673bf9e4
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-14 17:04:21 +0530

    Time: 83 ms (91.62%), Space: 40.3 MB (89.29%) - LeetHub

---
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int comp(int a,int b){
+        if(a==b){
+            return 0;
+        }else if(a>b){
+            return -1;
+        }
+        return 1;
+    }
+    
+    int maxTurbulenceSize(vector<int>& arr) {
+        int n = arr.size();
+        int l = 0;
+        int max_len = 1;
+        for(int r = 1; r < n; r++){
+            if(comp(arr[r],arr[r-1])==0){
+                max_len = max(max_len,(r-l));
+                l = r;
+            }else if( r == n-1 || comp(arr[r-1],arr[r])*comp(arr[r],arr[r+1]) != -1 ){
+                max_len = max(max_len,r-l+1);
+                l = r;
+            }
+        }
+        return max_len;
+    }
+};
