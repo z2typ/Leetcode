1c57589a5e7eb9edfdc6403e5489c2f10e5c663d
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-14 20:14:52 +0530

    Time: 4 ms (36.13%), Space: 7.5 MB (95.20%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n);
-        if(n==1) return nums[0];
-        else if(n==0) return 0;
         
-        int a = nums[0];
-        int b = max(nums[0],nums[1]);
-        
-        for(int i=2;i<n;i++){
-            int temp = max(a+nums[i], b);
-            a = b;
-            b = temp;
+        if(nums.size()==1){
+            return nums[0];
+        }else if(nums.size()>=2){
+            nums[1] = max(nums[0],nums[1]);
+        }else if(nums.size()==0){
+            return 0;
         }
-        
-        return max(a,b);
+        for(int i=2;i<nums.size();i++){
+            nums[i] = max(nums[i]+nums[i-2],nums[i-1]);
+        }
+
+        return nums.back();
     }
 };
