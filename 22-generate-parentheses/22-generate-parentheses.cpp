ef11ad00bf8d2722b49d5f3524a9787ceb72346b
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-14 23:46:42 +0530

    Time: 6 ms (59.12%), Space: 13.9 MB (33.15%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> ans;
+    void paren(int n, int open,int closed,string s=""){
+        if(closed==n){
+            ans.push_back(s);
+            return;
+        }
+        // if(closed>open){
+        //     return;
+        // }
+        
+        if(open==closed){
+            paren(n,open+1,closed,s+"(");
+        }else if(open>closed){
+            if(open<n)
+            paren(n,open+1,closed,s+"(");
+            paren(n,open,closed+1,s+")");
+        }
+        
+    }
+    vector<string> generateParenthesis(int n) {
+        paren(n,0,0);
+        return ans;
+    }
+};
