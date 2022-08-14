36d008dff6efa7dbfd7037399076383da5f2a4cb
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-14 20:48:53 +0530

    Time: 11 ms (6.90%), Space: 9.7 MB (80.36%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -14,8 +14,8 @@ public:
     TreeNode* invertTree(TreeNode* root) {
         if(root){
             swap(root->left,root->right);
-            invertTree(root->left);
-            invertTree(root->right);
+            root->left = invertTree(root->left);
+            root->right = invertTree(root->right);
         }
         return root;
     }
