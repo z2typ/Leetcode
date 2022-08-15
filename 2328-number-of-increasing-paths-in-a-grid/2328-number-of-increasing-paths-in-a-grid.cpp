fba0ffaa06843dbd52dcf08abb6dfc402c29104d
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-15 21:02:34 +0530

    Time: 454 ms (48.46%), Space: 57.5 MB (26.19%) - LeetHub

---
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -0,0 +1,53 @@
+#define MOD 1'000'000'007
+class Solution {
+public:
+    vector<vector<int>> visited;
+    
+    bool isSafe(vector<vector<int>> &matrix, int x,int y, int currVal = INT_MIN){
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        return 0<=x && x<n && 0<=y && y<m && (matrix[x][y] > currVal);
+    }
+    
+    int X[4] = {0,0,-1,1};
+    int Y[4] = {-1,1,0,0};
+    
+    inline int add(long long a,long long b){
+        return ((a%MOD)+(b%MOD))%MOD;
+    }
+    
+    int dfs(vector<vector<int>> &matrix, int x,int y){
+        
+        if(visited[x][y]){
+            return visited[x][y];
+        }
+        int paths = 0;
+        
+        for(int i=0;i<4;i++){                
+            if(isSafe(matrix, x+X[i], y+Y[i], matrix[x][y])){
+                paths = add(paths,dfs(matrix,x+X[i],y+Y[i])+1);
+                
+            }
+        }
+        
+        // if(paths==0){
+        //     paths = 1;
+        // }
+        
+        return visited[x][y] = paths;
+        
+    }
+    int countPaths(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        visited = vector<vector<int>>(n,vector<int>(m));
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                count = add(count, dfs(grid,i,j)+1);
+            }
+        }
+        return count;
+    }
+};
