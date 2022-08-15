18881eeda7815f80e0395c1fa11dfa830356a723
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-15 12:29:09 +0530

    Time: 105 ms (70.74%), Space: 34.5 MB (73.15%) - LeetHub

---
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int> count(26);
+        
+        for(char c : tasks){
+            count[c-'A']++;
+        }
+        
+        sort(count.begin(),count.end(),greater<int>());
+        
+        int max_ct = count[0];
+        int empty_slots = (max_ct-1)*n;
+        
+        for(int i=1;i<26;i++){
+            empty_slots -= min(max_ct-1,count[i]);
+        }
+        
+        return empty_slots > 0 ? empty_slots + tasks.size() : tasks.size(); 
+    }
+};
