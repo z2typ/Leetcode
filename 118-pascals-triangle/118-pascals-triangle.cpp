b571f348f858ce6b87e426c34d2a5d919014a92a
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-18 13:31:14 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.76%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        
+        ans.push_back({1});
+        
+        for(int i=1;i<numRows; i++){
+            vector<int> a;
+            a.push_back(1);
+            for(int j=1; j<i; j++){
+                a.push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+            a.push_back(1);
+            ans.push_back(a);
+        }
+        return ans;
+    }
+};
