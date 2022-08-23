1251f90dc7e527cae02b3010abdfdecbd07b1c29
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-23 21:06:43 +0530

    Time: 1186 ms (30.17%), Space: 135.4 MB (37.05%) - LeetHub

---
+++ 1851-minimum-interval-to-include-each-query/1851-minimum-interval-to-include-each-query.cpp
@@ -0,0 +1,64 @@
+struct Interval{
+    
+    int length;
+    int left;
+    int right;
+    
+    Interval(int left,int right){
+        this->left = left;
+        this->right = right;
+        this->length = (right-left+1);
+    }
+    
+    Interval(vector<int> &v){
+        this->left = v[0];
+        this->right = v[1];
+        this->length = (right-left+1);
+    }
+    
+    
+    bool operator >(const Interval &a) const{
+        if(this->length!=a.length){
+            return this->length > a.length;
+        }
+        return this->right > a.right;
+    }
+    
+};
+class Solution {
+public:
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+        // auto pq_comp = [](Interval &a, Interval &b){return b<a;};
+        priority_queue<Interval, vector<Interval>, greater<Interval> > pq;
+        unordered_map<int, vector<int>> index;
+        for(int i=0;i<queries.size();i++){
+            index[queries[i]].push_back(i);
+        }
+        sort(intervals.begin(),intervals.end());
+        sort(queries.begin(),queries.end());
+        vector<int> res(queries.size(),-1);
+        int query = queries[0];
+        
+        int i = 0;
+        for(int query : queries){
+            while(i < intervals.size() && intervals[i][0] <= query){
+                pq.push(Interval(intervals[i]));
+                i++;
+            }
+            
+            while(!pq.empty() && pq.top().right < query){
+                pq.pop();
+            }
+            
+            if(!pq.empty()){
+                for(int idx : index[query])
+                    res[idx] = pq.top().length;
+            }
+            
+        }
+        
+        return res;
+        
+        
+    }
+};
