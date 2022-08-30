cd5ac592d41e831aeaa6e64dd351c529ebe1d269
z2typ <102999319+z2typ@users.noreply.github.com>
2022-08-30 18:56:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (16.01%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,14 +3,16 @@ public:
     
     int climbStairs(int n) {
         vector<int> dp(n+1);
-        dp[0] = 1;
-        dp[1] = 1;
-        
+        int a = 1;
+        int b = 1;
+        int ans = 1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            ans  = a + b;
+            a = b;
+            b = ans;
         }
         
-        return dp[n];
+        return ans;
         
     }
 };
