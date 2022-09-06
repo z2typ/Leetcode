bdbb01d673837097042bea64bd49846108af7bb4
z2typ <102999319+z2typ@users.noreply.github.com>
2022-09-06 22:45:21 +0530

    Time: 8 ms (29.56%), Space: 6 MB (25.46%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long int l = 0, r = x*1ll;
-        while(l<r){
-            long int mid = l + (r-l+1)/2;
-            if (mid*mid <= x) {
-                l = mid;
-            }else{
+        long long l = 0, r = x;
+        
+        while(l<=r){
+            long long mid = l + (r - l)/2;
+            if(mid*mid == x){
+                return mid;
+            }else if(mid*mid > x){
                 r = mid - 1;
+            }else{
+                l = mid + 1;
             }
         }
         return r;
