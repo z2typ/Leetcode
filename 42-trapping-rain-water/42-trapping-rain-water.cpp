e0b5c208eaa2cfbda13f0ed07df35ef875bbf63d
z2typ <102999319+z2typ@users.noreply.github.com>
2022-09-11 13:23:38 +0530

    Time: 40 ms (11.66%), Space: 19.8 MB (61.24%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int l = 0, r = n-1;
-        int maxLeft = height[0], maxRight = height[n-1];
+        int l = 0 , r = n-1;
+        
+        int leftMax = height[l], rightMax = height[r];
+        
         int waterTrapped = 0;
-        while(l < r){
-            if(maxLeft < maxRight){
+        
+        while( l < r ){
+            if( leftMax < rightMax){
                 l += 1;
-                maxLeft = max(maxLeft,height[l]);
-                waterTrapped += maxLeft - height[l];
+                leftMax = max(height[l], leftMax);
+                waterTrapped += leftMax - height[l];
             }else{
                 r -= 1;
-                maxRight = max(maxRight,height[r]);
-                waterTrapped += maxRight - height[r];
+                rightMax = max(height[r], rightMax);
+                waterTrapped += rightMax - height[r];
             }
         }
+        
         return waterTrapped;
+        
     }
 };
