45bfc59cb7189d1a39724043ee484736095a67f6
z2typ <102999319+z2typ@users.noreply.github.com>
2022-09-22 21:33:57 +0530

    Time: 21 ms (62.37%), Space: 14.3 MB (14.25%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        vector<string> words;
         int j = 0;
+        string reversed_string = "";
+        string word;
         for(int i=0;i<s.length();i++){
             if(s[i]==' '){
-                words.push_back(s.substr(j,i-j));
+                word = s.substr(j,i-j);
+                reverse(word.begin(),word.end());
+                reversed_string += word+" ";
                 j = i+1;
             }
-        }
-        words.push_back(s.substr(j));
-        
-        for(string &word : words){
-            reverse(word.begin(),word.end());
-        }
-        string reversed_string = "";
-        
-        for(string word : words){
-            reversed_string += word+" ";
-        }
-        reversed_string.pop_back();
+        }   
+        word = s.substr(j);
+        reverse(word.begin(),word.end());
+        reversed_string += word;
         return reversed_string;
     }
 };
