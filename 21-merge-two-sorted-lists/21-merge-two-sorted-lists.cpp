5e3cf3b440a79d88668c3f06560e2cfd71efa404
z2typ <102999319+z2typ@users.noreply.github.com>
2022-10-01 12:08:08 +0530

    Time: 12 ms (61.45%), Space: 14.8 MB (81.28%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -12,31 +12,49 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode *p1=list1, *p2=list2;
-        ListNode *head = new ListNode();
+        ListNode *head = NULL;
         ListNode *temp = head;
         while(p1 && p2){
             if(p1->val < p2->val){
-                temp->next = p1;
-                p1 = p1->next;
-                temp = temp->next;
-                temp->next = NULL;
+                if(head==NULL){
+                    temp = p1;
+                    head = temp;
+                    p1 = p1->next;
+                    temp->next = NULL;
+                }else{
+                    temp->next = p1;
+                    p1 = p1->next;
+                    temp = temp->next;
+                    temp->next = NULL;
+                }
             }else{
-                temp->next = p2;
-                p2 = p2->next;
-                temp = temp->next;
-                temp->next = NULL;
+                if(head==NULL){
+                    temp = p2;
+                    head = temp;
+                    p2 = p2->next;
+                    temp->next = NULL;
+                }else{
+                    temp->next = p2;
+                    p2 = p2->next;
+                    temp = temp->next;
+                    temp->next = NULL;
+                }
             }
         }
         
         if(p1){
-            temp->next = p1;
+            if(head)
+                temp->next = p1;
+            else
+                return p1;
         }else{
-            temp->next = p2;
+            if(head)
+                temp->next = p2;
+            else
+                return p2;
         }
         
-        temp = head;
-        head = head->next;
-        delete temp;
+
         return head;
         
     }
