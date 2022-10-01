b398c9e3608bdafcc5c33a2a27e7fdc0be874655
z2typ <102999319+z2typ@users.noreply.github.com>
2022-10-01 19:44:00 +0530

    Time: 41 ms (34.97%), Space: 19.5 MB (59.74%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int x = nums[0];
+        int ct = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(x==nums[i]){
+                ct += 1;
+            }else{
+                ct--;
+                if(ct==-1){
+                    ct=1;
+                    x = nums[i];
+                }
+            }
+        }
+
+        return x;
+    }
+};
