f1ee0caaf8a2f050c60873c827aec50162dfb2e8
z2typ <102999319+z2typ@users.noreply.github.com>
2022-10-01 17:24:12 +0530

    Time: 82 ms (24.08%), Space: 12.6 MB (40.18%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,39 +1,37 @@
 class Solution {
 public:
-    bool isSafe(vector<vector<char>> &grid,int x,int y){
+    
+    vector<vector<bool>> visited;
+    
+    bool isSafe(vector<vector<char>> &grid, int x, int y){
         int n = grid.size();
         int m = grid[0].size();
-        return 0<=x && x<n && 0<=y && y<m && grid[x][y]=='1';
+        return 0<=x && x<n && 0<=y && y<m && grid[x][y]=='1' && !visited[x][y];
     }
     
-    int X[4] = {0,0,-1,1};
-    int Y[4] = {1,-1,0,0};
+    int X[4] = {1,0,-1,0};
+    int Y[4] = {0,1,0,-1};
     
-    void dfs(vector<vector<char>> &grid, int x,int y){
-        
-        grid[x][y] = '0';
-        
+    void dfs(vector<vector<char>> &grid, int x, int y){
+        visited[x][y] = true;
         for(int i=0;i<4;i++){
             if(isSafe(grid,x+X[i],y+Y[i])){
                 dfs(grid,x+X[i],y+Y[i]);
             }
         }
-        
     }
-    
     int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
+        visited = vector<vector<bool>>(n,vector<bool>(m));
         int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isSafe(grid,i,j)){
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(isSafe(grid, i,j)){
                     dfs(grid,i,j);
                     count += 1;
                 }
             }
         }
-        
         return count;
         
     }
