8c905e4ec8273ce26d067ffee0cd72db8bb90283
z2typ <102999319+z2typ@users.noreply.github.com>
2022-10-01 17:34:21 +0530

    Time: 115 ms (7.98%), Space: 27.5 MB (92.11%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0,r = nums.size()-1;
-        while(l<= r){
+        int l = 0, r = nums.size()-1;
+        while(l<=r){
             int mid = (l+r)/2;
             if(nums[mid]==target){
                 return mid;
-            }else if(nums[mid]<target){
+            }else if(nums[mid] < target){
                 l = mid+1;
             }else{
                 r = mid-1;
