224b3bb4675cc5151b9da088825aad5f95c970ad
z2typ <102999319+z2typ@users.noreply.github.com>
2022-10-04 17:28:59 +0530

    Time: 33 ms (55.58%), Space: 7.8 MB (69.12%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -15,6 +15,7 @@ public:
         int n = s.length();
         
         unordered_map<char, int> freq_t, freq_s;
+        int count = t.length();
         for(char ch : t){
             freq_t[ch] += 1;
         }
@@ -25,12 +26,20 @@ public:
             
             freq_s[s[r]] += 1;
             
-            while(isValidWindow(freq_t, freq_s)){
+            if(freq_t.count(s[r]) && freq_s[s[r]] <= freq_t[s[r]]){
+                count -= 1;
+            }
+            
+            while(count==0){
                 if(r-l+1 < minWindowLength){
                     minWindowLength = r-l+1;
                     start_idx = l;
                 }
+                
                 freq_s[s[l]] -= 1;
+                if( freq_t.count(s.at(l)) && freq_s.at(s.at(l)) < freq_t.at(s.at(l)) ){
+                    count += 1;
+                }
                 l += 1;
             }
         }
