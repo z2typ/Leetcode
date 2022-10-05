a8b2a4a14658ad0e42d93377029d6b6a1c804afc
z2typ <102999319+z2typ@users.noreply.github.com>
2022-10-05 10:59:31 +0530

    Time: 285 ms (35.11%), Space: 22.6 MB (39.59%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
+        int n = s.length();
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+
         
-        if(n==0) return 0;
-        
-        vector<vector<bool>> dp(n,vector<bool>(n));
-        int len = 0;
-        int left_idx = 0;
+        int maxLength = 1;
+        int start_idx = 0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                if(s[i]==s[j]){
-                    if(i==j||i-j==1||dp[i-1][j+1]){
-                        dp[i][j] =true;
-                        if(i-j+1 >= len){
-                            left_idx = j;
-                            len = (i-j+1);
-                        }
-                        
+            for(int j=0; j<i; j++){
+                if(s[i]==s[j] && (dp[j+1][i-1]==true || i-j==1 || i-1==j+1)){
+                    dp[j][i] = true;
+                    if(i-j+1 > maxLength){
+                        start_idx = j;
+                        maxLength = i-j+1;
                     }
                 }
             }
         }
-        
-
-        
-        return s.substr(left_idx,len);
+        return s.substr(start_idx,maxLength);
     }
 };
