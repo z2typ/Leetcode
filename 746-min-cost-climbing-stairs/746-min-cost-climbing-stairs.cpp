2cafaddac87fba5dcd16d64a0ef40894effb7f07
z2typ <102999319+z2typ@users.noreply.github.com>
2022-10-05 10:42:55 +0530

    Time: 12 ms (50.82%), Space: 13.8 MB (55.03%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,24 +1,10 @@
 class Solution {
 public:
-    int util(int n,vector<int>& cost,vector<int>& t){
-        if(t[n]!=-1) return t[n];
-        if(n==0){
-            return t[n] = 0;
-        }else if(n==1){
-            return t[n] =  0;
-        }else{
-            return t[n] = min(util(n-1,cost,t)+cost[n-1],util(n-2,cost,t)+cost[n-2]);
-        }
-        
-    }
     int minCostClimbingStairs(vector<int>& cost) {
-        
-        cost.push_back(0);
         int n = cost.size();
-        reverse(cost.begin(),cost.end());
-        vector<int> t(n+1,-1);
-        
-        return util(n,cost,t);
-        
+        for(int i=2; i<n; i++){
+            cost[i] = min(cost[i-1],cost[i-2])+cost[i];
+        }
+        return min(cost[n-2],cost[n-1]);
     }
 };
